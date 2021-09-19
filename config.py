5ae68b255da81b2193f8eb457c897d3d40c225fd
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-09-19 22:42:57 +0300

    added index.html

---
+++ config.py
@@ -0,0 +1,38 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    DEBUG = True
+    
+    config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
+}
