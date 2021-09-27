821d144ea51500e95a11f15ccd70f911d7470f71
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-09-27 16:24:59 +0300

    deplloymet

--- manage.py
+++ manage.py
@@ -3,9 +3,7 @@ from flask_script import Manager, Server
 from app.models import *
 from flask_migrate import Migrate, MigrateCommand
 
-# Creating app instance
-# app = create_app('test')
-# app = create_app('development')
+
 app = create_app('production')
 
 # Create manager instance
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MAIL_USERNAME='josephmbugus16@gmail.com'
+export MAIL_PASSWORD='1949joe@16'
+export SECRET_KEY='e4b204c968'
+python3.8 manage.py server
